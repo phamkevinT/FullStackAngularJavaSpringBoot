08159a0dd4cb8633137a075baf6308b6624c364f
Kevin Pham <phambamkevin@gmail.com>
2021-03-25 19:09:51 -0700

    REST API with Spring Data JPA

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/kevinpham/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.kevinpham.ecommerce.dao;
+
+import com.kevinpham.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+
+}
