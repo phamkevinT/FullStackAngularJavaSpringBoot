11fc5c06ec0dbd0ff782ac26759006a115691696
Kevin Pham <phambamkevin@gmail.com>
2021-03-25 19:37:33 -0700

    Configure for READ only

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/kevinpham/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/kevinpham/ecommerce/config/MyDataRestConfig.java
@@ -26,6 +26,5 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
     }
 }
