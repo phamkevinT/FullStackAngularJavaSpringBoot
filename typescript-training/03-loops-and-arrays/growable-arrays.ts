d3ace5d6d0af5edf680b2c36caa7a47c21993e24
Kevin Pham <phambamkevin@gmail.com>
2021-04-05 14:02:33 -0700

    relocated folders

--- typescript-training/03-loops-and-arrays/growable-arrays.ts
+++
@@ -1,9 +0,0 @@
-let sportsTwo: string[] = ["Golf", "Cricket", "Tennis"];
-
-sportsTwo.push("Swimming");
-sportsTwo.push("Football");
-
-for(let tempSport of sportsTwo) {
-    console.log(tempSport);
-    
-}
--- typescript-training/04-classes/05-modules/Driver.ts
+++
@@ -1,7 +0,0 @@
-import { Customer } from "./Customer";
-
-// create an instance 
-let myCustomer = new Customer("Kevin", "Pham");
-
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
