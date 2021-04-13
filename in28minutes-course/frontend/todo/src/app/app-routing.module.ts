12dc319cb0e20bd74f5f5c7707bfa321271b14ff
Kevin Pham <phambamkevin@gmail.com>
2021-04-12 17:08:21 -0700

    implemented logout component

--- in28minutes-course/frontend/todo/src/app/app-routing.module.ts
+++ in28minutes-course/frontend/todo/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path:'', component: LoginComponent },
-  { path:'login', component: LoginComponent },
-  { path:'welcome/:name', component: WelcomeComponent},
-  { path:'todos', component: ListTodosComponent},
-  { path:'**', component: ErrorComponent}
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome/:name', component: WelcomeComponent },
+  { path: 'todos', component: ListTodosComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', component: ErrorComponent }
 
 ];
 
--- in28minutes-course/frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ in28minutes-course/frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -26,4 +26,9 @@ export class HardcodedAuthenticationService {
     return !(user === null);
   }
 
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser');
+  }
+
 }
