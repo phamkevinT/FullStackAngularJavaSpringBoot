71421d341f6396569aa017e3f033fb5bdc59b8a1
Kevin Pham <phambamkevin@gmail.com>
2021-06-30 14:51:04 -0700

    Login and Logout Component with Okta

---
+++ luv2code-course/ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa133p5k8NEHb6av5d7',
+        issuer: 'https://dev-67478206.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
